c9c500cf61f9bdd5e3142511f6b4e94914128448
kodenysio-63 <korzun.denys_io63@edu.kpi.ua>
2026-09-20 20:17:00 +0300

    Add files via upload

---
+++ Laboratorky/lab1/lab1task1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    float x = -2.0f;
+    float y;
+
+    if (x >= 8.0f) {
+        if (x < 23.0f) {
+            y = -5.0f * x * x * x + 10.0f;
+            printf("%f = %f\n", x, y);
+        } else {
+            printf("Функція не визначена для x = %f\n", x);
+        }
+    } else if (x < -19.0f) {
+        y = 2.0f * x * x + 8.0f * x * x;
+        printf("%f = %f\n", x, y);
+    } else if (x > -3.0f) {
+        if (x <= 0.0f) {
+            y = 2.0f * x * x + 8.0f * x * x;
+            printf("%f = %f\n", x, y);
+        } else {
+            printf("Функція не визначена для x = %f\n", x);
+        }
+    } else {
+        printf("Функція не визначена для x = %f\n", x);
+    }
+
+
+    return 0;
+}
