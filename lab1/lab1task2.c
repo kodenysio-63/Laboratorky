2002ccf874a33ef2a87725d7cbcd3e0d0331f3d1
kodenysio-63 <korzun.denys_io63@edu.kpi.ua>
2026-09-23 13:08:38 +0300

    /

---
+++ lab1/lab1task2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float x = -10.0f;
+    float y;
+
+    if (x >= 8.0f && x < 23.0f) {
+        y = -5.0f * x * x * x + 10.0f;
+        printf("%f = %f\n", x, y);
+    } else if (x < -19.0f || (x > -3.0f && x <= 0.0f)) {
+        y = 2.0f * x * x * x + 8.0f * x * x;
+        printf("%f = %f\n", x, y);
+    } else {
+        printf("Функція не визначена для x = %f\n", x);
+    }
+    return 0;
+}
